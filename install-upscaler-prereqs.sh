ce15161d64d0f953442c7db40092d25414f07f8d
darkcristle <85809726+darkcristle@users.noreply.github.com>
2023-09-10 18:10:15 -0700

    Update install-upscaler-prereqs.sh

--- install-upscaler-prereqs.sh
+++ install-upscaler-prereqs.sh
@@ -13,4 +13,4 @@ echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
+sudo apt-get update -Y
