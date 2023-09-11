e47844deee16541f280f3738cd01b3bc144616c9
darkcristle <85809726+darkcristle@users.noreply.github.com>
2023-09-10 18:11:53 -0700

    Update install-upscaler-prereqs.sh

--- install-upscaler-prereqs.sh
+++ install-upscaler-prereqs.sh
@@ -2,8 +2,8 @@
 
 ##-----Install Docker-----##
 # Add Docker's official GPG key:
-sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg
+sudo apt-get -y update
+sudo apt-get -y install ca-certificates curl gnupg
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
@@ -13,4 +13,4 @@ echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update -Y
+sudo apt-get -y update
